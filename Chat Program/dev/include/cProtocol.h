e2f4b1bb4ecf09d2df96405e953cc4abd2dea5fd
Ivan Parkhomenko <allic.gator@gmail.com>
2019-10-07 01:12:38 -0500

    Fixed wierd merge error

--- Chat Program/dev/include/cProtocol.h
+++ Chat Program/dev/include/cProtocol.h
@@ -5,13 +5,8 @@
 class Protocol
 {
 private:
-<<<<<<< HEAD
 	Buffer buffer;
 	unsigned int message_id;
-=======
-	Buffer buffer = Buffer(0);
-	unsigned int message_id = 0;
->>>>>>> bb40fb4bae4a42550fd5306ae094cf2a054cb03e
 
 public:
 	Protocol()
@@ -26,21 +21,18 @@ public:
 		message_id = m_id;
 	}
 
-<<<<<<< HEAD
 	std::vector<uint8_t> GetBuffer()
 	{
 		return buffer.GetBufferContent();
 	}
 
 	std::vector<uint8_t> UserSendMessage(std::string room, std::string message)
-=======
-	Buffer SendMessage(std::string room, std::string message)
->>>>>>> bb40fb4bae4a42550fd5306ae094cf2a054cb03e
 	{
 		buffer.Clear();
 
 		// [Header] [length] [room_name] [length] [message]
 		buffer.writeInt32LE(INT_SIZE * 4 + room.length() + message.length());
+		//buffer.
 		buffer.writeInt32LE(INT_SIZE, message_id++);
 
 		//room
@@ -51,11 +43,7 @@ public:
 		buffer.writeInt32LE(INT_SIZE * 3 + room.length(), message.length());
 		buffer.WriteString(INT_SIZE * 4 + room.length(), message);
 
-<<<<<<< HEAD
 		return GetBuffer();
-=======
-		return buffer;
->>>>>>> bb40fb4bae4a42550fd5306ae094cf2a054cb03e
 	}
 
 	std::vector<uint8_t> UserRecieveMessage(std::string name, std::string room, std::string message)
@@ -64,6 +52,7 @@ public:
 
 		// [Header] [length] [name] [length] [room_name] [length] [message]
 		buffer.writeInt32LE(INT_SIZE * 5 + name.length() + room.length() + message.length());
+		//buffer.
 		buffer.writeInt32LE(INT_SIZE, message_id++);
 
 		//name
@@ -81,45 +70,20 @@ public:
 		return GetBuffer();
 	}
 
-<<<<<<< HEAD
 	std::vector<uint8_t> UserJoinRoom(std::string room)
-=======
-	Buffer JoinRoom(std::string name, std::string room)
-	{
-		buffer.Clear();
-
-		// [Header] [length] [user_name] [length] [room_name]
-		buffer.writeInt32LE(INT_SIZE * 3 + room.length());
-		buffer.writeInt32LE(INT_SIZE, message_id++);
-
-		//name
-		buffer.writeInt32LE(INT_SIZE * 2, name.length());
-		buffer.WriteString(INT_SIZE * 3, name);
-
-		//room
-		buffer.writeInt32LE(INT_SIZE * 3 + name.length(), room.length());
-		buffer.WriteString(INT_SIZE * 4 + name.length(), room);
-
-		return buffer;
-	}
-
-	Buffer LeaveRoom(std::string room)
->>>>>>> bb40fb4bae4a42550fd5306ae094cf2a054cb03e
 	{
 		buffer.Clear();
 
 		// [Header] [length] [room_name]
+		//packet length
 		buffer.writeInt32LE(INT_SIZE * 3 + room.length());
+		//message_id
 		buffer.writeInt32LE(INT_SIZE, message_id++);
 
 		buffer.writeInt32LE(INT_SIZE * 2, room.length());
 		buffer.WriteString(INT_SIZE * 3, room);
 
-<<<<<<< HEAD
 		return GetBuffer();
-=======
-		return buffer;
->>>>>>> bb40fb4bae4a42550fd5306ae094cf2a054cb03e
 	}
 
 	std::vector<uint8_t> UserLeaveRoom(std::string room)
