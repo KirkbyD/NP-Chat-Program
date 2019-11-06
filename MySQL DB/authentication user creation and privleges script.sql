f0c7eaf4210f1c1be639c8ceede711478191fd3a
Brian <bmcowan900@gmail.com>
2019-11-06 13:21:28 -0500

    fixed auth serv user privliges

--- MySQL DB/authentication user creation and privleges script.sql
+++ MySQL DB/authentication user creation and privleges script.sql
@@ -4,6 +4,4 @@ CREATE USER IF NOT EXISTS 'auth_serv'@'localhost'
     REQUIRE SSL
 ;
 
-GRANT SELECT ON authservdb.* TO 'auth_serv'@'localhost';
-GRANT UPDATE ON authservdb.* TO 'auth_serv'@'localhost';
-GRANT DELETE ON authservdb.* TO 'auth_serv'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON authservdb.* TO 'auth_serv'@'localhost';
