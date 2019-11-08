88270473888e76b86c36021dd2463bdfeb7047af
Brian <bmcowan900@gmail.com>
2019-11-08 18:01:46 -0500

    added login authentication

--- Chat Program/Authentication Server/auth_server.cpp
+++ Chat Program/Authentication Server/auth_server.cpp
@@ -389,23 +389,13 @@ int main(int argc, char** argv)
 
 								// insert new user into db
 								try {
-									//pstmt = con->prepareStatement("INSERT INTO employees (Employee, Department_ID) VALUES('Brian', 5);");
-
 									std::stringstream ss;
-
 									std::string usrn = Registration->username();
 									std::string psw = Registration->password();
 									std::string email = Registration->email();
 									unsigned long requester = Registration->requestid();
 									std::string salt = "";
-
 									unsigned uid;
-
-									// hash password
-									// create salt
-									// append hashed password to salt
-									// send password
-									// send salt
 
 									// preform cleaning on the user names here.
 									prepstmt = con->prepareStatement("INSERT INTO users (username, creation_date) VALUES('" + usrn + "', NOW())");
@@ -477,8 +467,123 @@ int main(int argc, char** argv)
 								break;
 							}
 							case EMAILAUTH:
+								int newpacket_length = buf.readInt32LE(INT_SIZE * 0);
+								int newmessage_id = buf.readInt32LE(INT_SIZE * 1);
+								int newmessage_length = buf.readInt32LE(INT_SIZE * 2);
+								std::string newmessage = buf.ReadString(INT_SIZE * 3, newmessage_length);
+
+								printf("Size of message: %i\n", newpacket_length);
+								printf("MESSAGE ID: %i\n", newmessage_id);
+								printf("MESSAGE: %s\n", newmessage.c_str());
+
+								AuthenticateAccount* AuthLogin = new AuthenticateAccount();
+
+								AuthLogin->ParseFromString(newmessage);
+
+								std::cout << AuthLogin->requestid() << std::endl;
+								std::cout << AuthLogin->identifier() << std::endl;
+								std::cout << AuthLogin->password() << std::endl;
+
+								try {
+									std::stringstream ss;
+									unsigned long requester = AuthLogin->requestid();
+									std::string usrn = AuthLogin->identifier();
+									std::string psw = AuthLogin->password();
+									std::string salt = "";
+									std::string hpsw = "";
+
+									// preform cleaning on the user names here.
+									ss << "SELECT password, salt FROM authservdb.web_auth wa JOIN authservdb.users u ON u.ID = wa.User_ID WHERE u.username = '" << usrn << "'";
+									prepstmt = con->prepareStatement(ss.str());
+									rslt = prepstmt->executeQuery();
+
+									if (rslt != 0) {
+										while (rslt->next()) {
+											hpsw = rslt->getString(1);
+											salt = rslt->getString(2);
+										}
+									}
+									else {
+										// user does not exist
+									}
+
+									std::string hpsw;
+									psw = hash_pass(psw);
+
+
+									if ((salt + hpsw) == (salt + psw)) {
+										// login credentials accepted
+									}
+									else {
+										// login credentials rejected
+									}
+
+								}
+								catch (sql::SQLException& exception) {
+									DisplayError(exception);
+								}
+
+
 								break;
 							case USERNAMEAUTH:
+								int newpacket_length = buf.readInt32LE(INT_SIZE * 0);
+								int newmessage_id = buf.readInt32LE(INT_SIZE * 1);
+								int newmessage_length = buf.readInt32LE(INT_SIZE * 2);
+								std::string newmessage = buf.ReadString(INT_SIZE * 3, newmessage_length);
+
+								printf("Size of message: %i\n", newpacket_length);
+								printf("MESSAGE ID: %i\n", newmessage_id);
+								printf("MESSAGE: %s\n", newmessage.c_str());
+
+								RegisterAccount* Registration = new RegisterAccount();
+
+								Registration->ParseFromString(newmessage);
+
+								std::cout << Registration->requestid() << std::endl;
+								std::cout << Registration->email() << std::endl;
+								std::cout << Registration->password() << std::endl;
+
+								try {
+									std::stringstream ss;
+									unsigned long requester = AuthLogin->requestid();
+									std::string email = AuthLogin->identifier();
+									std::string psw = AuthLogin->password();
+									std::string salt = "";
+									std::string hpsw = "";
+									std::string usrn = "";
+
+								// preform cleaning on the user names here.
+								ss << "SELECT password, salt, username FROM authservdb.web_auth wa JOIN authservdb.users u ON u.ID = wa.User_ID WHERE wa.email = '" << email << "'";
+								prepstmt = con->prepareStatement(ss.str());
+								rslt = prepstmt->executeQuery();
+
+								if (rslt != 0) {
+									while (rslt->next()) {
+										hpsw = rslt->getString(1);
+										salt = rslt->getString(2);
+										usrn = rslt->getString(3);
+									}
+								}
+								else {
+									// email does not exist
+								}
+
+								std::string hpsw;
+								psw = hash_pass(psw);
+
+
+								if ((salt + hpsw) == (salt + psw)) {
+									// login credentials accepted
+								}
+								else {
+									// login credentials rejected
+								}
+
+								}
+								catch (sql::SQLException& exception) {
+									DisplayError(exception);
+								}
+
 								break;
 							default:
 								printf("You what mate?\n");
